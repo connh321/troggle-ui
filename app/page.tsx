277f0d1f97a1801e82beff663dfde63b4cd3125b
Connor Hunter <connh321@gmail.com>
2025-08-30 15:46:34 -0400

    Removed Material UI / SASS for Tailwind CSS

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,5 @@ import styles from './page.module.css';
 import { ReactElement } from 'react';
 
 export default function Home(): ReactElement {
-  return <div className={styles.page}></div>;
+  return <div className={styles.page}>test</div>;
 }
